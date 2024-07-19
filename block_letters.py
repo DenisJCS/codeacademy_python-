870cbbb9363c921428172ed028bd2795ea57a8ef
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-18 21:55:01 -0400

    Create block_letters.py

---
+++ block_letters.py
@@ -0,0 +1,9 @@
+print("DDDD   K     K")
+print("D   D  K    K")
+print("D   D  K  K")
+print("D   D  KK   ")
+print("D   D  K  K ")
+print("D   D  K    K")
+print("DDDD   K      K")
+#Comment : My first name is from Greek and my last name from my graandfather
+#fan fact: I like cold to dress warm
