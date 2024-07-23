bae93cf16de0db8db7b7e1801dab3fcb60e770d4
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-22 21:27:44 -0400

    Create travel.py

---
+++ travel.py
@@ -0,0 +1,29 @@
+# Write your code below:
+
+def trip_planner(first_destination, second_destination,final_destination="Codecademy HQ"):
+ print("Here is what your trip will look like!")
+ print("First, we will stop in " + first_destination + " , then " + second_destination + ", and lastly " + final_destination )
+ 
+trip_planner("London", "India", "New Zeland")
+trip_planner("France", "Germany", "Denmark")
+trip_planner("Denmark", "France","Germany")
+trip_planner(first_destination = "Iceland", final_destination = "Germany", second_destination = "India")
+trip_planner("Brooklyn", "Queens")
+
+Here is what your trip will look like!
+First, we will stop in London , then India, and lastly New Zeland
+
+Here is what your trip will look like!
+First, we will stop in France , then Germany, and lastly Denmark
+
+Here is what your trip will look like!
+First, we will stop in Denmark , then France, and lastly Germany
+
+Here is what your trip will look like!
+First, we will stop in Iceland , then India, and lastly Germany
+
+Here is what your trip will look like!
+First, we will stop in Brooklyn , then Queens, and lastly Codecademy HQ
+
+
+
