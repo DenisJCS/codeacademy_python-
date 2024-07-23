83bee7af4ba51a00263ef4022eaa1642ae849350
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-23 10:55:39 -0400

    Update travel.py

--- travel.py
+++ travel.py
@@ -27,3 +27,48 @@ First, we will stop in Brooklyn , then Queens, and lastly Codecademy HQ
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#TripPlanner V1.0
+def trip_planner_welcome(name):
+   print("Welcome to tripplanner v1.0 :"+ name)
+trip_planner_welcome("<Your Name Here>")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(2.43)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport = "Car"):
+ print("Your trip start off in " + origin)
+ print("And you are traveling to " + destination)
+ print("You will be traveling by " + mode_of_transport)
+ print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("Frace","London",estimate)
+
+
+  
+
+
+
+
+
+
