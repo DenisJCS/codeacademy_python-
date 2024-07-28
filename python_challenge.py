15375747aefc0ef3f5d6c75039e0de78b044b6ee
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-28 15:03:57 -0400

    Create python_challenge.py

---
+++ python_challenge.py
@@ -0,0 +1,11 @@
+num1 = 6
+num2 = 3
+
+# Write your if statement here
+if num1+num2 !=10:
+  not_ten = True
+else:
+  not_ten = False
+
+# Uncomment the below lines to show the result
+# print("Does the sum of the numbers equal 10? " + str(not_ten))
