1d1d5ff0281f80765a74a73324e0c8d61caf0d0a
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-28 15:11:12 -0400

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -9,3 +9,25 @@ else:
 
 # Uncomment the below lines to show the result
 # print("Does the sum of the numbers equal 10? " + str(not_ten))
+
+# Monthly budget
+budget = 2000
+
+# Monthly expenses
+food_bill = 200
+electricity_bill = 100
+internet_bill = 60
+rent = 1500
+
+# Calculate the total amount of expenses
+total = food_bill + electricity_bill + internet_bill + rent
+print(total)
+# Check if the total is greater than the budget and store the result in over_budget
+if total > budget :
+  over_budget = True
+else:
+  over_budget = False
+
+# Uncomment the below lines to see the results
+print(over_budget)
+print("\nTotal : " + str(total))
