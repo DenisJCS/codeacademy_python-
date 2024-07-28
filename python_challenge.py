828a5a8ac51a9ca414a38533565c863ae3282e64
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-28 15:32:55 -0400

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -43,3 +43,12 @@ def large_power (base , exponent):
 
 result = large_power(5000, 1)
 print(result)
+
+# Write your twice_as_large function here:
+def twice_as_large(num1, num2):
+  if num1 > num2*2:
+    return True
+  else:
+    return False
+result = twice_as_large (100,25)
+print(result)
