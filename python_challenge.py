1079e10771039993b1f81c9e4b774b2b21f05adc
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-31 10:24:12 -0400

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -135,4 +135,16 @@ print(movie_review(4))
 print(movie_review(6))
 # should print "This one was fun."
 
-"Challenges are done"
+#Write your function here
+def append_size(my_list):
+  my_list.append(len(my_list))
+  return my_list
+
+
+
+#Uncomment the line below when your function is done
+append_size([23, 42, 108])
+
+#print(append_size([23, 42, 108]))
+print(append_size([23, 42 , 108]))
+
