b8f0daa069e42dd7b8e6954dcc13ad6e7afd1a0e
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-31 10:11:27 -0400

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -87,3 +87,50 @@ print(result)
 # should print True
 #print(same_name("Tina", "Amber"))
 # should print False
+
+
+# Write your max_num function here:
+def max_num(num1, num2, num3):
+  if num1>num2 and num1>num3:
+   return num1
+  elif num2>num1 and num2>num3:
+    return num2
+  elif num3>num1 and num3>num2:
+    return num3
+  else:
+    return "It's a tie!"
+# Uncomment these function calls to test your max_num function:
+#print(max_num(-10, 0, 10))
+print(max_num(-10, 0, 10))
+# should print 10
+#print(max_num(-10, 5, -30))
+print(max_num(-10, 5, -30))
+# should print 5
+#print(max_num(-5, -10, -10))
+print(max_num(-5, -10, -10))
+# should print -5
+#print(max_num(2, 3, 3))
+print(max_num(2, 3, 3))
+# should print "It's a tie!"
+
+
+# Write your movie_review function here:
+def movie_review(rating):
+  if (rating <= 5):
+    return "Avoid at all costs!"
+  if (rating <9):
+    return "This one was fun"
+  return "Outstanding"
+
+
+
+# Uncomment these function calls to test your movie_review function:
+#print(movie_review(9))
+print(movie_review(9))
+# should print "Outstanding!"
+#print(movie_review(4))
+print(movie_review(4))
+# should print "Avoid at all costs!"
+#print(movie_review(6))
+print(movie_review(6))
+# should print "This one was fun."
