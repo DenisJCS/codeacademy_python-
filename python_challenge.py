c04d4ad2f59a34d4227357f7696e20611df2f3fb
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-07-31 10:39:57 -0400

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -168,6 +168,17 @@ def larger_list(my_list, my_list2):
 #print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
 print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
 
+#Write your function here
+def more_than_n(my_list, item, n):
+  if my_list.count(item) > n:
+    return True
+  else:
+    return False
+
+#Uncomment the line below when your function is done
+#print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3))
+
 
 #Uncomment the line below when your function is done
 #print(append_sum([1, 1, 2]))
