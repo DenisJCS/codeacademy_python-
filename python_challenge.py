a1b2cb8b85643357d44c831b9dbdae6d585d784a
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-03 10:46:19 -0400

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -190,6 +190,13 @@ def every_three_nums(start):
 # print(every_three_nums(91))
 print(every_three_nums(91)
 
+      #Write your function here
+def remove_middle(my_list, start,end):
+  return my_list[:start] + my_list[end+1:]
+#Uncomment the line below when your function is done
+# print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+print(remove_middle([4,8,15,16,23,42],1, 3))
+
 #Uncomment the line below when your function is done
 #print(append_sum([1, 1, 2]))
 print(append_sum([1,1,2]))
