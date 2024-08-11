189455c6a31190cf316e04704a8da19efd91e096
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-10 22:42:22 -0400

    Update loops_chalange.py

--- loops_chalange.py
+++ loops_chalange.py
@@ -1,1 +1,17 @@
+#Write your function here
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+  for number in lst1:
+   sum1 += number
+  for number in lst2:
+   sum2 += number
+  if sum1 >= sum2:
+    return lst1
+  else:
+    return lst2
 
+
+#Uncomment the line below when your function is done
+#print(larger_sum([1, 9, 5], [2, 3, 7]))
+print(larger_sum([1, 9, 5], [2, 3, 7]))
