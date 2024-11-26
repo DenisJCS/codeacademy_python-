ca7e8f77a7acad29675ed3bb0b3f7a309496afdc
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-11-26 10:50:22 -0500

    Update strings.py

--- strings.py
+++ strings.py
@@ -13,3 +13,13 @@ Thus having prepared their buds
 against a sure winter
 the wise trees
 stand sleeping in the cold.
+
+  # Replcace method in use
+  toomer_bio = \
+"""
+Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career, was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman, born into slavery in 1839 in Chatham County, North Carolina. Jean Tomer is most well known for his first book Cane, which vividly portrays the life of African-Americans in southern farmlands.
+"""
+
+toomer_bio_fixed = toomer_bio.replace('Tomer', 'Toomer')
+print(toomer_bio_fixed)
+
