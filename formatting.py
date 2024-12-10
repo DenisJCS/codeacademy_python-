303fb0f9e3fcc561c442199d2b0b63bbc90a7100
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-12-10 15:25:24 -0500

    Create formatting.py

---
+++ formatting.py
@@ -0,0 +1,16 @@
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(publishing_date = publishing_date, author = author, title = title, original_work = original_work)
+  return poem_desc
+
+author = "Shel Silverstein"
+title = "My Beard"
+original_work = "Where the Sidewalk Ends"
+publishing_date = "1974"
+
+my_beard_description = poem_description(
+  author, title, original_work, publishing_date
+)
+
+print(my_beard_description)
+
+The poem Where the Sidewalk Ends by My Beard was originally published in 1974 in Shel Silverstein.
