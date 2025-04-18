ead52c713ee50408f490cfbf3600a3bcd36b3b10
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-04-18 16:02:28 -0400

    Update module_and_form.py

--- module_and_form.py
+++ module_and_form.py
@@ -9,3 +9,19 @@ randomer_number = random.choice(random_list)
 
 # Print randomer_number below:
 print(randomer_number)
+
+
+import codecademylib3_seaborn
+
+# Add your code below:
+from matplotlib import pyplot as plt
+import random 
+
+numbers_a = range(1, 13)
+
+numbers_b = random.sample(range(1000), 12)
+print(numbers_b)
+
+plt.plot(numbers_a, numbers_b)
+plt.show()
+
