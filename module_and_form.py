878f3593fe52b123cdab975fc6831610bd3ba5a4
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-04-18 16:10:39 -0400

    Update module_and_form.py

--- module_and_form.py
+++ module_and_form.py
@@ -25,3 +25,13 @@ print(numbers_b)
 plt.plot(numbers_a, numbers_b)
 plt.show()
 
+
+# Import Decimal below:
+from decimal import Decimal
+
+# Fix the floating point math below:
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
